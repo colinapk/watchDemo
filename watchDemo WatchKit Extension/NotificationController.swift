e3aa696f3101b3f587ffd98e849d0ea50a3d5b0b
Colin <appdev-ios@audi.com.cn>
2015-01-05 11:22:34 +0800

    initial commit

---
+++ watchDemo WatchKit Extension/NotificationController.swift
@@ -0,0 +1,53 @@
+//
+//  NotificationController.swift
+//  watchDemo WatchKit Extension
+//
+//  Created by Colin on 15/1/5.
+//  Copyright (c) 2015年 colin. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    /*
+    override func didReceiveLocalNotification(localNotification: UILocalNotification, withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
+        // This method is called when a local notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification inteface as quickly as possible.
+        //
+        // After populating your dynamic notification interface call the completion block.
+        completionHandler(.Custom)
+    }
+    */
+    
+    /*
+    override func didReceiveRemoteNotification(remoteNotification: [NSObject : AnyObject], withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
+        // This method is called when a remote notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification inteface as quickly as possible.
+        //
+        // After populating your dynamic notification interface call the completion block.
+        completionHandler(.Custom)
+    }
+    */
+}
