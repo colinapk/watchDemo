fa2df72fdca3fed5ddc7a2b790dcd7468d71b96d
Colin <appdev-ios@audi.com.cn>
2015-01-06 17:05:07 +0800

    single ton

---
+++ watchDemo WatchKit Extension/DemoPlayer.swift
@@ -0,0 +1,54 @@
+//
+//  DemoPlayer.swift
+//  watchDemo
+//
+//  Created by Colin on 15/1/6.
+//  Copyright (c) 2015年 colin. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+private var _instance = DemoPlayer()
+
+class DemoPlayer: NSObject {
+    private var myPlayer:AVAudioPlayer?
+    private var curSongIndex = 1
+    
+    private let audioFiles = ["你是我心爱的姑娘_汪峰","Where Are We Now?_David","Just One Last Dance"]
+    private let covers = ["wangfeng","david","sarah"]
+    
+    class func shareInstance()->DemoPlayer{
+        return _instance
+    }
+    func curSongCover()->UIImage{
+        let imageName = covers[curSongIndex]
+        let imagePath = NSBundle.mainBundle().pathForResource(imageName, ofType: "png")
+        let image = UIImage(contentsOfFile: imagePath!)
+        return image!
+    }
+    func curSongTitle()->NSString{
+        return audioFiles[curSongIndex]
+    }
+    func playCurSong(){
+        myPlayer = self.loadSound(audioFiles[curSongIndex])
+        myPlayer!.play()
+    }
+    func playNextSong(){
+        changeCurSongIndex()
+        playCurSong()
+    }
+    private func changeCurSongIndex(){
+        if(++curSongIndex > 2){
+            curSongIndex = 0
+        }
+    }
+    private func loadSound(filename:NSString) -> AVAudioPlayer {
+        let url = NSBundle.mainBundle().URLForResource(filename, withExtension: "mp3")
+        var error:NSError? = nil
+        let player = AVAudioPlayer(contentsOfURL: url, error: &error)
+        player.prepareToPlay()
+        return player
+    }
+   
+}
--- watchDemo WatchKit Extension/InterfaceController.swift
+++ watchDemo WatchKit Extension/InterfaceController.swift
@@ -8,16 +8,16 @@
 
 import WatchKit
 import Foundation
-import AVFoundation
+
+
+
 
 class InterfaceController: WKInterfaceController {
 
     @IBOutlet weak var songCover: WKInterfaceImage!
-    var myPlayer:AVAudioPlayer?
     
-    var curSongIndex = 1
-    let audioFiles = ["你是我心爱的姑娘_汪峰","Where Are We Now?_David","Just One Last Dance"]
-    let covers = ["wangfeng","david","sarah"]
+ 
+    
     
     
     override func awakeWithContext(context: AnyObject?) {
@@ -29,43 +29,21 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-        playCurSong()
+        DemoPlayer.shareInstance().playCurSong()
+        self.songCover.setImage(DemoPlayer.shareInstance().curSongCover())
         
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
-        myPlayer!.stop()
+//        myPlayer!.stop()
         
     }
 
     @IBAction func onButtonClicked() {
-        changeCurSong()
-        playCurSong()
-        
+        DemoPlayer.shareInstance().playNextSong()
+        self.songCover.setImage(DemoPlayer.shareInstance().curSongCover())
     }
-    func playCurSong(){
-        let imageName = covers[curSongIndex]
-        let imagePath = NSBundle.mainBundle().pathForResource(imageName, ofType: "png")
-        let image = UIImage(contentsOfFile: imagePath!)
-        
-        self.songCover.setImage(image)
-        
-        myPlayer = self.loadSound(audioFiles[curSongIndex])
-        myPlayer!.play()
-    }
-    func changeCurSong(){
-        
-        if(++curSongIndex > 2){
-            curSongIndex = 0
-        }
-    }
-    func loadSound(filename:NSString) -> AVAudioPlayer {
-        let url = NSBundle.mainBundle().URLForResource(filename, withExtension: "mp3")
-        var error:NSError? = nil
-        let player = AVAudioPlayer(contentsOfURL: url, error: &error)
-        player.prepareToPlay()
-        return player
-    }
+
 }
