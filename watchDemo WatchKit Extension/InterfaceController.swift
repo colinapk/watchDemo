08d9a6632bb28224b431a9aea55913ec45a90b48
Colin <appdev-ios@audi.com.cn>
2015-01-06 15:24:52 +0800

    play file

--- watchDemo WatchKit Extension/InterfaceController.swift
+++ watchDemo WatchKit Extension/InterfaceController.swift
@@ -8,13 +8,15 @@
 
 import WatchKit
 import Foundation
-
+import AVFoundation
 
 class InterfaceController: WKInterfaceController {
 
     @IBOutlet weak var songCover: WKInterfaceImage!
-    var curSongIndex = 0
-    let audioFiles = ["你是我心爱的姑娘_汪峰.mp3","Where Are We Now?_David.mp3","Just One Last Dance.mp3"]
+    var myPlayer:AVAudioPlayer?
+    
+    var curSongIndex = 1
+    let audioFiles = ["你是我心爱的姑娘_汪峰","Where Are We Now?_David","Just One Last Dance"]
     let covers = ["wangfeng","david","sarah"]
     
     
@@ -27,24 +29,43 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        playCurSong()
+        
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+        myPlayer!.stop()
+        
     }
 
     @IBAction func onButtonClicked() {
         changeCurSong()
+        playCurSong()
+        
+    }
+    func playCurSong(){
         let imageName = covers[curSongIndex]
+        let imagePath = NSBundle.mainBundle().pathForResource(imageName, ofType: "png")
+        let image = UIImage(contentsOfFile: imagePath!)
         
-        self.songCover.setImageNamed(imageName)
+        self.songCover.setImage(image)
         
+        myPlayer = self.loadSound(audioFiles[curSongIndex])
+        myPlayer!.play()
     }
     func changeCurSong(){
         
         if(++curSongIndex > 2){
             curSongIndex = 0
         }
+    }
+    func loadSound(filename:NSString) -> AVAudioPlayer {
+        let url = NSBundle.mainBundle().URLForResource(filename, withExtension: "mp3")
+        var error:NSError? = nil
+        let player = AVAudioPlayer(contentsOfURL: url, error: &error)
+        player.prepareToPlay()
+        return player
     }
 }
